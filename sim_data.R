2b2ed042e69a36b694795d29d68ca43997b29ebc
John Flournoy <uoflournoy@gmail.com>
2020-03-09 11:35:05 -0400

    update

--- sim_data.R
+++ sim_data.R
@@ -37,42 +37,43 @@ invisible(lapply(1:dim(y_sim)[1], function(i){
   return(NULL)
 }))
 
-#time ./mlmed_example sample algorithm=hmc engine=nuts max_depth=15 num_samples=1000 num_warmup=1000 output file=fit.csv data file=data_to_fit.R
+#bash cmdstan_chain_per_sim.bash
 
-fit <- rstan::read_stan_csv('../cmdstan/mlmed/fit.csv')
-
-params <- tidybayes::gather_draws(gend, `(.*_(a|b|cp|dy|dm|ty|tm).*|(a|b|cp|dy|dm|ty|tm))`, regex = TRUE)
-cor_params <- tidybayes::gather_draws(gend, `.*Omega.*`, regex = TRUE)
-tau_params <- tidybayes::gather_draws(gend, `.*Tau.*`, regex = TRUE)
-
-params_from_sim <- tidybayes::gather_draws(fit, `(.*_(a|b|cp|dy|dm|ty|tm).*|(a|b|cp|dy|dm|ty|tm))`, regex = TRUE)
-
-cor_params_from_sim <- tidybayes::gather_draws(fit, `.*Omega.*`, regex = TRUE)
-tau_params_from_sim <- tidybayes::gather_draws(fit, `.*Tau.*`, regex = TRUE)
-
-v_pars <- params_from_sim %>% group_by(.variable) %>%
+if(file.exists('fit01.csv')){
+  fit <- rstan::read_stan_csv('fit01.csv')
+  
+  params <- tidybayes::gather_draws(gend, `(.*_(a|b|cp|dy|dm|ty|tm).*|(a|b|cp|dy|dm|ty|tm))`, regex = TRUE)
+  cor_params <- tidybayes::gather_draws(gend, `.*Omega.*`, regex = TRUE)
+  tau_params <- tidybayes::gather_draws(gend, `.*Tau.*`, regex = TRUE)
+  
+  params_from_sim <- tidybayes::gather_draws(fit, `(.*_(a|b|cp|dy|dm|ty|tm).*|(a|b|cp|dy|dm|ty|tm))`, regex = TRUE)
+  
+  cor_params_from_sim <- tidybayes::gather_draws(fit, `.*Omega.*`, regex = TRUE)
+  tau_params_from_sim <- tidybayes::gather_draws(fit, `.*Tau.*`, regex = TRUE)
+  
+  v_pars <- params_from_sim %>% group_by(.variable) %>%
     summarize(mean(.value)) %>%
     left_join(params[params$.iteration == 1,]) %>%
     filter(grepl('v_', .variable)) %>%
     extract(.variable, c('var', 'var2', 'idx'), regex = '(v_)(.*)\\.(.*)')
-
-v_pars %>%
+  
+  v_pars %>%
     ggplot(aes(x = .value, y = `mean(.value)`)) +
     geom_point() +
     geom_smooth(method = 'lm') +
     facet_wrap(~var2, scales = 'free') +
     geom_abline(intercept = 0, slope = 1)
-
-qplot(v_pars$.value[v_pars$var2 == 'dm'], v_pars$`mean(.value)`[v_pars$var2 == 'b'],
-      geom = c('point', 'smooth'), method = 'lm') +
+  
+  qplot(v_pars$.value[v_pars$var2 == 'dm'], v_pars$`mean(.value)`[v_pars$var2 == 'b'],
+        geom = c('point', 'smooth'), method = 'lm') +
     geom_abline(intercept = 0, slope = 1)
-
-qplot(v_pars$.value[v_pars$var2 == 'tm'], v_pars$`mean(.value)`[v_pars$var2 == 'b'],
-      geom = c('point', 'smooth'), method = 'lm') +
+  
+  qplot(v_pars$.value[v_pars$var2 == 'tm'], v_pars$`mean(.value)`[v_pars$var2 == 'b'],
+        geom = c('point', 'smooth'), method = 'lm') +
     geom_abline(intercept = 0, slope = 1)
-
-
-params_from_sim %>% group_by(.variable) %>%
+  
+  
+  params_from_sim %>% group_by(.variable) %>%
     summarize(mean(.value)) %>%
     left_join(params[params$.iteration == 1,]) %>%
     filter(grepl('^u_', .variable)) %>%
@@ -82,27 +83,29 @@ params_from_sim %>% group_by(.variable) %>%
     geom_smooth(method = 'lm') +
     facet_wrap(~var2, scales = 'free') +
     geom_abline(intercept = 0, slope = 1)
-
-params_from_sim %>% group_by(.variable) %>%
+  
+  params_from_sim %>% group_by(.variable) %>%
     summarize(mean(.value)) %>%
     left_join(params[params$.iteration == 1,]) %>%
     filter(!grepl('^(v|u)_', .variable)) %>%
     ggplot(aes(x = .value, y = `mean(.value)`)) +
     geom_point() +
     geom_smooth(method = 'lm')
-
-params_from_sim %>% group_by(.variable) %>%
+  
+  params_from_sim %>% group_by(.variable) %>%
     summarize(mean(.value)) %>%
     left_join(params[params$.iteration == 1,]) %>%
     filter(grepl('tau', .variable)) %>%
     ggplot(aes(x = .value, y = `mean(.value)`)) +
     geom_point() +
     geom_smooth(method = 'lm')
-
-cor_params_from_sim %>% group_by(.variable) %>%
+  
+  cor_params_from_sim %>% group_by(.variable) %>%
     summarize(mean(.value)) %>%
     left_join(cor_params[cor_params$.iteration == 1,]) %>% View
-
-tau_params_from_sim %>% group_by(.variable) %>%
+  
+  tau_params_from_sim %>% group_by(.variable) %>%
     summarize(mean(.value)) %>%
     left_join(tau_params[tau_params$.iteration == 1,]) %>% View
+}
+
